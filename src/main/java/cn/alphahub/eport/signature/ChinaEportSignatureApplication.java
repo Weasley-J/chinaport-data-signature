1daba73eb5acce5af53d9f64a8440c67e1eeb532
Weasley J <1432689025@qq.com>
2022-02-17 18:03:33 +0800

    Code Optimization.

--- src/main/java/cn/alphahub/eport/signature/ChinaEportSignatureApplication.java
+++ src/main/java/cn/alphahub/eport/signature/ChinaEportSignatureApplication.java
@@ -2,7 +2,6 @@ package cn.alphahub.eport.signature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 中国电子口岸（海口海关CEBXxxMessage）加签服务
@@ -10,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author Weasley J
  * @date 2022-02-13
  */
-@EnableScheduling
 @SpringBootApplication
 public class ChinaEportSignatureApplication {
     public static void main(String[] args) {
