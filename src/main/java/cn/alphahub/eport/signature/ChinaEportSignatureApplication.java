422dee6ea995ca44555a9c123c15dbff58f14fd1
Weasley J <1432689025@qq.com>
2022-03-06 17:36:01 +0800

    1. opt

--- src/main/java/cn/alphahub/eport/signature/ChinaEportSignatureApplication.java
+++ src/main/java/cn/alphahub/eport/signature/ChinaEportSignatureApplication.java
@@ -3,7 +3,6 @@ package cn.alphahub.eport.signature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 中国电子口岸（海口海关CEBXxxMessage）加签服务
@@ -11,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author Weasley J
  * @date 2022-02-13
  */
-@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ChinaEportSignatureApplication {
