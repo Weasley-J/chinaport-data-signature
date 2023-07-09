010153ad2278728f106b3e54d6a71b74f0a0ce3e
Weasley J <1432689025@qq.com>
2023-07-09 13:42:06 +0800

    - 增加取算法类型测试

--- chinaport-data-signature-app/src/test/java/cn/alphahub/eport/signature/client/StandardSignHandlerTest.java
+++ chinaport-data-signature-app/src/test/java/cn/alphahub/eport/signature/client/StandardSignHandlerTest.java
@@ -5,10 +5,17 @@ import cn.alphahub.eport.signature.config.UkeyInitialConfigTest;
 import cn.alphahub.eport.signature.core.SignHandler;
 import cn.alphahub.eport.signature.entity.SignRequest;
 import cn.alphahub.eport.signature.entity.SignResult;
+import cn.alphahub.eport.signature.entity.UkeyRequest;
 import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.LinkedHashMap;
+
+import static cn.alphahub.dtt.plus.util.JacksonUtil.toJson;
 
 
 /**
@@ -21,12 +28,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 @SpringBootTest
 class StandardSignHandlerTest {
+    @Autowired
+    private SignHandler client;
 
     @Test
     void sendInitDataAsPEMWithHashPTest() {
         SignRequest request = new SignRequest(UkeyInitialConfigTest.CEB621Message);
         String parameter = UkeyInitialConfig.getSignDataAsPEMParameter(request);
-        SignHandler client = new SignHandler();
         SignResult result = client.sign(request, parameter);
         System.out.println(JSONUtil.toJsonPrettyStr(result));
     }
@@ -35,7 +43,6 @@ class StandardSignHandlerTest {
     void sendInitDataNoHashAsPEMParameterTest() {
         SignRequest request = new SignRequest(UkeyInitialConfigTest.CEB621Message);
         String parameter = UkeyInitialConfig.getSignDataNoHashAsPEMParameter(request);
-        SignHandler client = new SignHandler();
         SignResult result = client.sign(request, parameter);
         System.out.println(JSONUtil.toJsonPrettyStr(result));
     }
@@ -47,8 +54,27 @@ class StandardSignHandlerTest {
     void ukeyVersion() {
         String method = "cus-sec_SpcGetCardAttachInfo";
         SignRequest request = new SignRequest(UkeyInitialConfigTest.CEB621Message);
-        SignHandler client = new SignHandler();
         SignResult result = client.sign(request, "666");
         System.out.println(JSONUtil.toJsonPrettyStr(result));
     }
+
+    /**
+     * 取算法类型（取加密算法标识）
+     *
+     * @implNote lReader 取指定卡的信息，1取RA卡，其它值则取被制卡或KEY
+     */
+    @Test
+    @DisplayName("取算法类型（取加密算法标识）")
+    void ukeyEncryptionAlgorithmIdentification() {
+        String method = "ra_GetCryptAlgo";
+        UkeyRequest ukeyRequest = new UkeyRequest(method, new LinkedHashMap<>() {{
+            put("inData", "");
+            put("lReader", 1);
+        }});
+        String signParams = toJson(ukeyRequest);
+        SignRequest request = new SignRequest("");
+        SignResult result = client.sign(request, signParams);
+        System.out.println(JSONUtil.toJsonPrettyStr(result));
+    }
+
 }
