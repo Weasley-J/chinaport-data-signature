1ce52fce058236f630d4b8c2511b47222f11c3a2
Weasley J <1432689025@qq.com>
2023-07-30 19:38:46 +0800

    [feature] 移除“ds:SignedInfo”节点的第一行XML申明

--- chinaport-data-signature-app/src/main/java/cn/alphahub/eport/signature/core/WebSocketClientHandler.java
+++ chinaport-data-signature-app/src/main/java/cn/alphahub/eport/signature/core/WebSocketClientHandler.java
@@ -28,6 +28,8 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.concurrent.locks.LockSupport;
 
+import static cn.alphahub.eport.signature.util.XMLUtils.removeFirstLineOfSourceXml;
+
 /**
  * 加签websocket客户端基类
  * <ul><li>全参数构造函数注入IOC</li></ul>
@@ -91,7 +93,7 @@ public class WebSocketClientHandler extends TextWebSocketHandler {
                     webSocketWrapper.getSignResult().setCertNo(responseArgs.getData().get(1));
                     if (SignHandler.isSignXml(webSocketWrapper.getRequest())) {
                         webSocketWrapper.getSignResult().setX509Certificate(certificateHandler.getX509Certificate(response.get_method()));
-                        webSocketWrapper.getSignResult().setSignatureNode(ChinaEportReportClient.buildSignedInfoNode(webSocketWrapper.getSignResult()));
+                        webSocketWrapper.getSignResult().setSignatureNode(removeFirstLineOfSourceXml(ChinaEportReportClient.buildSignedInfoNode(webSocketWrapper.getSignResult())));
                     }
                 } else {
                     sendAlertSignFailure(JacksonUtil.toPrettyJson(response));
