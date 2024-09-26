2357faf153124d864509f3d1d5a7face38265ae4
Weasley J <1432689025@qq.com>
2024-09-26 16:02:53 +0800

    feat: 优化证书下载接口描述

--- chinaport-data-signature-webapp/src/main/java/cn/alphahub/eport/signature/controller/rpc/EportCertController.java
+++ chinaport-data-signature-webapp/src/main/java/cn/alphahub/eport/signature/controller/rpc/EportCertController.java
@@ -5,17 +5,16 @@ import cn.alphahub.eport.signature.core.SignHandler;
 import cn.alphahub.eport.signature.entity.UkeyRequest;
 import cn.alphahub.eport.signature.entity.UkeyResponse.Args;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 
 import static cn.alphahub.eport.signature.core.CertificateHandler.SING_DATA_METHOD;
 
@@ -38,8 +37,9 @@ public class EportCertController {
     /**
      * 下载证书
      *
-     * @apiNote 证书文件格式: 证书编号.cer, 遇到项目启动的首页下载证书出现文件名为 unknown.cer 的情况将下载链接复制到浏览器中打开
+     * @apiNote 证书文件格式: 证书编号.cer, 遇到项目启动的首页下载证书出现文件名为 unknown.cer 的情况将下载链接复制到浏览器中打开，有公司反馈x509证书导出还存在问题， <a href="http://tool.qdhuaxun.cn/">先去这里导</a> ：海关179号公告对接 => ukey证书导出工具
      * @download
+     * @since 20240926
      */
     @GetMapping("/cert/download")
     public void downloadX509Certificate(HttpServletResponse response) throws IOException {
